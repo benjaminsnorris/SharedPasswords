4b82e6f7203c18135e94dae8031faa9d17e98153
Ben Norris <code@bsn.design>
2016-08-03 14:58:39 -0600

    Tweak service to be more generic

--- SharedPasswords/SharedPasswordService.swift
+++ SharedPasswords/SharedPasswordService.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 import ReSwift
-import Marshal
+
+// MARK: - Actions
+
+struct SharedPasswordError: Action {
+    var error: ErrorType
+}
+
+struct SharedPasswordRetrieved: Action, CustomStringConvertible {
+    var username: String
+    var password: String
+    
+    var description: String {
+        return "SharedPasswordRetrieved(email:\(username))"
+    }
+}
+
 
 struct SharedPasswordService {
     
@@ -20,40 +35,33 @@ struct SharedPasswordService {
     }
     
     
-    // MARK: - Internal properties
-    
-    var store = AppState.sharedStore
-    
-    
     // MARK: - Internal functions
     
-    func requestCredentials(state: AppState, store: Store<AppState>) -> Action? {
+    func requestCredentials<T: StateType>(state: T, store: Store<T>) -> Action? {
         SecRequestSharedWebCredential(nil, nil) { credentials, error in
             dispatch_async(dispatch_get_main_queue()) {
                 guard error == nil else {
-                    self.store.dispatch(SharedPasswordError(error: error!))
+                    store.dispatch(SharedPasswordError(error: error!))
                     return
                 }
                 guard let unwrappedCredentials = credentials else {
-                    self.store.dispatch(SharedPasswordError(error: Error.nilCredentials))
+                    store.dispatch(SharedPasswordError(error: Error.nilCredentials))
                     return
                 }
                 let arrayCredentials = unwrappedCredentials as [AnyObject]
                 guard let typedCredentials = arrayCredentials as? [[String: AnyObject]] else {
-                    self.store.dispatch(SharedPasswordError(error: Error.malformedCredentials))
+                    store.dispatch(SharedPasswordError(error: Error.malformedCredentials))
                     return
                 }
                 guard let credential = typedCredentials.first else {
-                    self.store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+                    store.dispatch(SharedPasswordError(error: Error.missingCredentials))
                     return
                 }
-                do {
-                    let username: String = try credential <| String(kSecAttrAccount)
-                    let password: String = try credential <| String(kSecSharedPassword)
-                    self.store.dispatch(SharedPasswordRetrieved(username: username, password: password))
-                } catch {
-                    self.store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+                guard let username = credential[String(kSecAttrAccount)] as? String, password = credential[String(kSecSharedPassword)] as? String else {
+                    store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+                    return
                 }
+                store.dispatch(SharedPasswordRetrieved(username: username, password: password))
             }
         }
         return nil
