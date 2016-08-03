df34c5bf4feaf7089af5f1cab7e5d8bb9383cddc
Ben Norris <code@bsn.design>
2016-08-03 14:46:48 -0600

    Create blank framework

---
+++ SharedPasswords/SharedPasswords.h
@@ -0,0 +1,19 @@
+//
+//  SharedPasswords.h
+//  SharedPasswords
+//
+//  Created by Ben Norris on 8/3/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedPasswords.
+FOUNDATION_EXPORT double SharedPasswordsVersionNumber;
+
+//! Project version string for SharedPasswords.
+FOUNDATION_EXPORT const unsigned char SharedPasswordsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedPasswords/PublicHeader.h>
+
+
