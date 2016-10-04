b340a67da25d52d47b8b576eb86a4ee906fb997d
Ben Norris <code@bsn.design>
2016-10-04 15:45:14 -0600

    Migrate

--- SharedPasswords/SharedPasswordService.swift
+++ SharedPasswords/SharedPasswordService.swift
@@ -12,9 +12,9 @@ import OnePasswordExtension
 // MARK: - Actions
 
 public struct SharedPasswordError: Action {
-    public var error: ErrorType
+    public var error: Error
     
-    public init(error: ErrorType) {
+    public init(error: Error) {
         self.error = error
     }
 }
@@ -70,7 +70,7 @@ public struct SharedPasswordService {
     
     // MARK: - Enums
     
-    public enum Error: ErrorType {
+    public enum SharedPasswordServiceError: Error {
         case nilCredentials
         case malformedCredentials
         case missingCredentials
@@ -84,34 +84,34 @@ public struct SharedPasswordService {
     
     // MARK: - Constants
     
-    private static let sharedCredentialsKey = "sharedCredentials"
+    fileprivate static let sharedCredentialsKey = "sharedCredentials"
     
     
     // MARK: - Public functions
     
-    public func requestCredentials<T: StateType>(for domain: String? = nil) -> (state: T, store: Store<T>) -> Action? {
+    public func requestCredentials<T: StateType>(for domain: String? = nil) -> (_ state: T, _ store: Store<T>) -> Action? {
         return { state, store in
-            SecRequestSharedWebCredential(domain, nil) { credentials, error in
-                dispatch_async(dispatch_get_main_queue()) {
+            SecRequestSharedWebCredential(domain as CFString?, nil) { credentials, error in
+                DispatchQueue.main.async {
                     guard error == nil else {
                         store.dispatch(SharedPasswordError(error: error!))
                         return
                     }
                     guard let unwrappedCredentials = credentials else {
-                        store.dispatch(SharedPasswordError(error: Error.nilCredentials))
+                        store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.nilCredentials))
                         return
                     }
                     let arrayCredentials = unwrappedCredentials as [AnyObject]
                     guard let typedCredentials = arrayCredentials as? [[String: AnyObject]] else {
-                        store.dispatch(SharedPasswordError(error: Error.malformedCredentials))
+                        store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.malformedCredentials))
                         return
                     }
                     guard let credential = typedCredentials.first else {
-                        store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+                        store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.missingCredentials))
                         return
                     }
-                    guard let username = credential[String(kSecAttrAccount)] as? String, password = credential[String(kSecSharedPassword)] as? String else {
-                        store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+                    guard let username = credential[String(kSecAttrAccount)] as? String, let password = credential[String(kSecSharedPassword)] as? String else {
+                        store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.missingCredentials))
                         return
                     }
                     store.dispatch(SharedPasswordRetrieved(username: username, password: password))
@@ -122,11 +122,11 @@ public struct SharedPasswordService {
         }
     }
     
-    public func saveSharedCredentialIfNeeded<T: StateType>(for urlString: String, username: String, password: String) -> (state: T, store: Store<T>) -> Action? {
+    public func saveSharedCredentialIfNeeded<T: StateType>(for urlString: String, username: String, password: String) -> (_ state: T, _ store: Store<T>) -> Action? {
         return { state, store in
             var savedCredentials = [String: [String: AnyObject]]()
             var savedDomainCredentials = [String: AnyObject]()
-            if let credentials = NSUserDefaults.standardUserDefaults().objectForKey(SharedPasswordService.sharedCredentialsKey) as? [String: [String: AnyObject]] {
+            if let credentials = UserDefaults.standard.object(forKey: SharedPasswordService.sharedCredentialsKey) as? [String: [String: AnyObject]] {
                 savedCredentials = credentials
                 if let domainCredentials = savedCredentials[urlString] {
                     savedDomainCredentials = domainCredentials
@@ -135,14 +135,14 @@ public struct SharedPasswordService {
                     }
                 }
             }
-            SecAddSharedWebCredential(urlString, username, password) { error in
+            SecAddSharedWebCredential(urlString as CFString, username as CFString, password as CFString?) { error in
                 if let error = error {
                     store.dispatch(SharedPasswordError(error: error))
                     print("status=failed-to-add-shared-credential error=\(error) domain=\(urlString) username=\(username)")
                 } else {
-                    savedDomainCredentials[username] = true
+                    savedDomainCredentials[username] = true as AnyObject?
                     savedCredentials[urlString] = savedDomainCredentials
-                    NSUserDefaults.standardUserDefaults().setObject(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
+                    UserDefaults.standard.set(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
                     store.dispatch(SharedPasswordSaved(username: username, domain: urlString))
                 }
             }
@@ -150,25 +150,25 @@ public struct SharedPasswordService {
         }
     }
     
-    public func removeSharedCredentials<T: StateType>(for urlString: String, username: String) -> (state: T, store: Store<T>) -> Action? {
+    public func removeSharedCredentials<T: StateType>(for urlString: String, username: String) -> (_ state: T, _ store: Store<T>) -> Action? {
         return { state, store in
             var savedCredentials = [String: [String: AnyObject]]()
             var savedDomainCredentials = [String: AnyObject]()
-            if let credentials = NSUserDefaults.standardUserDefaults().objectForKey(SharedPasswordService.sharedCredentialsKey) as? [String: [String: AnyObject]] {
+            if let credentials = UserDefaults.standard.object(forKey: SharedPasswordService.sharedCredentialsKey) as? [String: [String: AnyObject]] {
                 savedCredentials = credentials
                 if let domainCredentials = savedCredentials[urlString] {
                     savedDomainCredentials = domainCredentials
                 }
             }
 
-            SecAddSharedWebCredential(urlString, username, nil) { error in
+            SecAddSharedWebCredential(urlString as CFString, username as CFString, nil) { error in
                 if let error = error {
                     store.dispatch(SharedPasswordError(error: error))
                     print("status=failed-to-add-shared-credential error=\(error) domain=\(urlString) username=\(username)")
                 } else {
-                    savedDomainCredentials.removeValueForKey(username)
+                    savedDomainCredentials.removeValue(forKey: username)
                     savedCredentials[urlString] = savedDomainCredentials
-                    NSUserDefaults.standardUserDefaults().setObject(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
+                    UserDefaults.standard.set(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
                     store.dispatch(SharedPasswordSaved(username: username, domain: urlString))
                 }
             }
@@ -176,19 +176,19 @@ public struct SharedPasswordService {
         }
     }
     
-    public func findLoginFrom1Password<T: StateType>(with urlString: String, viewController: UIViewController, button: AnyObject) -> (state: T, store: Store<T>) -> Action? {
+    public func findLoginFrom1Password<T: StateType>(with urlString: String, viewController: UIViewController, button: AnyObject) -> (_ state: T, _ store: Store<T>) -> Action? {
         return { state, store in
-            OnePasswordExtension.sharedExtension().findLoginForURLString(urlString, forViewController: viewController, sender: button) { loginDictionary, error in
+            OnePasswordExtension.shared().findLogin(forURLString: urlString, for: viewController, sender: button) { loginDictionary, error in
                 guard error == nil else {
                     store.dispatch(SharedPasswordError(error: error!))
                     return
                 }
-                guard let loginDictionary = loginDictionary as? [String: AnyObject] where loginDictionary.count > 0 else {
-                    store.dispatch(SharedPasswordError(error: Error.malformedCredentials))
+                guard let loginDictionary = loginDictionary as? [String: AnyObject] , loginDictionary.count > 0 else {
+                    store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.malformedCredentials))
                     return
                 }
-                guard let username = loginDictionary[AppExtensionUsernameKey] as? String, password = loginDictionary[AppExtensionPasswordKey] as? String else {
-                    store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+                guard let username = loginDictionary[AppExtensionUsernameKey] as? String, let password = loginDictionary[AppExtensionPasswordKey] as? String else {
+                    store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.missingCredentials))
                     return
                 }
                 store.dispatch(SharedPasswordRetrieved(username: username, password: password))
@@ -197,24 +197,24 @@ public struct SharedPasswordService {
         }
     }
     
-    public func createLoginIn1Password<T: StateType>(for urlString: String, appTitle: String, username: String? = nil, password: String? = nil, viewController: UIViewController, button: AnyObject) -> (state: T, store: Store<T>) -> Action? {
+    public func createLoginIn1Password<T: StateType>(for urlString: String, appTitle: String, username: String? = nil, password: String? = nil, viewController: UIViewController, button: AnyObject) -> (_ state: T, _ store: Store<T>) -> Action? {
         return { state, store in
             let username = username ?? ""
             let password = password ?? ""
             let loginDetails: [String: AnyObject] = [
-                AppExtensionTitleKey: appTitle,
-                AppExtensionUsernameKey: username,
-                AppExtensionPasswordKey: password,
+                AppExtensionTitleKey: appTitle as AnyObject,
+                AppExtensionUsernameKey: username as AnyObject,
+                AppExtensionPasswordKey: password as AnyObject,
             ]
-            OnePasswordExtension.sharedExtension().storeLoginForURLString(urlString, loginDetails: loginDetails, passwordGenerationOptions: nil, forViewController: viewController, sender: button) { loginDictionary, error in
-                dispatch_async(dispatch_get_main_queue()) {
-                    if let loginDictionary = loginDictionary where error == nil {
-                        guard let username = loginDictionary[AppExtensionUsernameKey] as? String, password = loginDictionary[AppExtensionPasswordKey] as? String else {
-                            store.dispatch(SharedPasswordError(error: Error.missingCredentials))
+            OnePasswordExtension.shared().storeLogin(forURLString: urlString, loginDetails: loginDetails, passwordGenerationOptions: nil, for: viewController, sender: button) { loginDictionary, error in
+                DispatchQueue.main.async {
+                    if let loginDictionary = loginDictionary , error == nil {
+                        guard let username = loginDictionary[AppExtensionUsernameKey] as? String, let password = loginDictionary[AppExtensionPasswordKey] as? String else {
+                            store.dispatch(SharedPasswordError(error: SharedPasswordServiceError.missingCredentials))
                             return
                         }
                         store.dispatch(SharedPasswordCreated(username: username, password: password))
-                    } else if let error = error where error.code != Int(AppExtensionErrorCodeCancelledByUser) {
+                    } else if let error = error as? NSError, error.code != Int(AppExtensionErrorCodeCancelledByUser) {
                         store.dispatch(SharedPasswordError(error: error))
                     }
                 }
@@ -223,38 +223,38 @@ public struct SharedPasswordService {
         }
     }
     
-    public func presentResetSharedCredentials<T: StateType>(for urlString: String, username: String?, viewController: UIViewController, sender: AnyObject) -> (state: T, store: Store<T>) -> Action? {
+    public func presentResetSharedCredentials<T: StateType>(for urlString: String, username: String?, viewController: UIViewController, sender: AnyObject) -> (_ state: T, _ store: Store<T>) -> Action? {
         return { state, store in
             var savedCredentials = [String: [String: AnyObject]]()
             var savedDomainCredentials = [String: AnyObject]()
-            if let credentials = NSUserDefaults.standardUserDefaults().objectForKey(SharedPasswordService.sharedCredentialsKey) as? [String: [String: AnyObject]] {
+            if let credentials = UserDefaults.standard.object(forKey: SharedPasswordService.sharedCredentialsKey) as? [String: [String: AnyObject]] {
                 savedCredentials = credentials
                 if let domainCredentials = savedCredentials[urlString] {
                     savedDomainCredentials = domainCredentials
                 }
             }
 
-            let alert = UIAlertController(title: NSLocalizedString("Reset shared credentials", comment: "Title for action sheet to reset shared credentials when logging in"), message: NSLocalizedString("This is typically not needed.\n\nBy default, the app will only save your shared credentials once, as subsequent attempts force you to confirm a password update whether it has changed or not.\n\nIf necessary, you can reset all shared credentials, or reset a specific username to force the credentials to save.", comment: "Explanation for action sheet to reset shared credentials"), preferredStyle: .ActionSheet)
-            if let username = username where username.characters.count > 0 {
-                alert.addAction(UIAlertAction(title: NSLocalizedString("Force password update", comment: "Action title"), style: .Default) { action in
-                    savedDomainCredentials.removeValueForKey(username)
+            let alert = UIAlertController(title: NSLocalizedString("Reset shared credentials", comment: "Title for action sheet to reset shared credentials when logging in"), message: NSLocalizedString("This is typically not needed.\n\nBy default, the app will only save your shared credentials once, as subsequent attempts force you to confirm a password update whether it has changed or not.\n\nIf necessary, you can reset all shared credentials, or reset a specific username to force the credentials to save.", comment: "Explanation for action sheet to reset shared credentials"), preferredStyle: .actionSheet)
+            if let username = username , username.characters.count > 0 {
+                alert.addAction(UIAlertAction(title: NSLocalizedString("Force password update", comment: "Action title"), style: .default) { action in
+                    savedDomainCredentials.removeValue(forKey: username)
                     savedCredentials[urlString] = savedDomainCredentials
-                    NSUserDefaults.standardUserDefaults().setObject(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
+                    UserDefaults.standard.set(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
                     store.dispatch(self.removeSharedCredentials(for: urlString, username: username))
                 })
             }
-            alert.addAction(UIAlertAction(title: NSLocalizedString("Reset all credentials", comment: "Action title"), style: .Destructive) { action in
-                savedCredentials.removeValueForKey(urlString)
-                NSUserDefaults.standardUserDefaults().setObject(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("Reset all credentials", comment: "Action title"), style: .destructive) { action in
+                savedCredentials.removeValue(forKey: urlString)
+                UserDefaults.standard.set(savedCredentials, forKey: SharedPasswordService.sharedCredentialsKey)
             })
-            alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel button title"), style: .Cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel button title"), style: .cancel, handler: nil))
             if let barButtonItem = sender as? UIBarButtonItem {
                 alert.popoverPresentationController?.barButtonItem = barButtonItem
             } else if let view = sender as? UIView {
                 alert.popoverPresentationController?.sourceView = view.superview
                 alert.popoverPresentationController?.sourceRect = view.frame
             }
-            viewController.presentViewController(alert, animated: true, completion: nil)
+            viewController.present(alert, animated: true, completion: nil)
             return nil
         }
     }
